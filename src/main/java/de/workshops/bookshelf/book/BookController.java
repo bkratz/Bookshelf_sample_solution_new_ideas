53f5950e225dee0e8fe719792d00f2e70d6d00bd
Birgit Kratz <mail@birgitkratz.de>
2023-10-21 20:50:47 +0200

    Issue 77: separate class for author

--- src/main/java/de/workshops/bookshelf/book/BookController.java
+++ src/main/java/de/workshops/bookshelf/book/BookController.java
@@ -3,12 +3,12 @@ package de.workshops.bookshelf.book;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,18 +17,26 @@ import java.util.List;
 public class BookController {
 
     private final ObjectMapper mapper;
+    private final ResourceLoader resourceLoader;
 
     private List<Book> books;
 
-    public BookController(ObjectMapper mapper) {
+    public BookController(ObjectMapper mapper, ResourceLoader resourceLoader) {
         this.mapper = mapper;
+        this.resourceLoader = resourceLoader;
     }
 
     @PostConstruct
     public void init() throws IOException {
-        this.books = mapper.readValue(new File("target/classes/books.json"), new TypeReference<>() {});
+        this.books = mapper.readValue(
+                resourceLoader
+                        .getResource("classpath:books.json")
+                        .getInputStream(),
+                new TypeReference<>() {
+                }
+        );
     }
-    
+
     @GetMapping
     public String getAllBooks(Model model) {
         model.addAttribute("books", books);
