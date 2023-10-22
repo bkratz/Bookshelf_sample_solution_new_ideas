56eaa779f3b39c932bff023fd43e5ed9decc8cb6
Birgit Kratz <mail@birgitkratz.de>
2023-10-22 03:25:20 +0200

    Issue 77: separate class for author

--- src/test/java/de/workshops/bookshelf/EnvironmentTest.java
+++ src/test/java/de/workshops/bookshelf/EnvironmentTest.java
@@ -3,18 +3,20 @@ package de.workshops.bookshelf;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest
+@ActiveProfiles("test-prod")
 class EnvironmentTest {
 
 	@Value("${server.port}")
 	private int port;
 
 	@Test
-	@EnabledIf(expression = "#{environment['spring.profiles.active'] == 'test-prod'}", loadContext = true)
+	@EnabledIf(expression = "#{environment.acceptsProfiles('test-prod', 'someotherprofile')}", loadContext = true)
 	void verifyProdPort() {
 		assertEquals(8090, port);
 	}
